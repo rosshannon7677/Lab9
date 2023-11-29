5543593fbd3800f0f84ed03670e2b6369af5a472
Ross Hannon <G00381859@gmit.ie>
2023-11-29 13:43:56 +0000

    L8 deep copy not fully working

--- Lab9.py
+++ Lab9.py
@@ -1,6 +1,7 @@
 # Imports
 import cv2
 import numpy as np
+import copy
 from matplotlib import pyplot as plt
 
 # Load the original image
@@ -10,15 +11,18 @@ imgOrig = cv2.imread(r'C:\Lab9\ATU1.jpg')
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
 # Perform Harris corner detection
-dst = cv2.cornerHarris(imgGray, blockSize, aperture_size, k)
 blockSize = 2
 aperture_size = 3
 k = 0.04
+dst = cv2.cornerHarris(imgGray, blockSize, aperture_size, k)
 
-# Mark the corners on the original image
-imgOrig[dst > 0.01 * dst.max()] = [0, 0, 255]  # Red color for corners
+# Create a deep copy of the original image
+imgHarris = copy.deepcopy(imgOrig)
+
+# Mark the corners on the deep copy
+imgHarris[dst > 0.01 * dst.max()] = [0, 0, 255]  # Red color for corners
 
 # Plot the result
-plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
 plt.title('Harris Corner Detection')
 plt.show()
