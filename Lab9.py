2e72205ba7eb969d596088efa23a21fd30c8517b
Ross Hannon <G00381859@gmit.ie>
2023-12-07 14:56:24 +0000

    L9 Dail vs Kitchen matching

--- Lab9.py
+++ Lab9.py
@@ -27,7 +27,36 @@ matches_bf = sorted(matches_bf, key=lambda x: x.distance)
 # Draw matches
 img_matches_bf = cv2.drawMatches(img1, kp1, img2, kp2, matches_bf[:10], None, flags=2)
 
-# Display the result
-plt.imshow(img_matches_bf)
-plt.title('BruteForceMatcher')
+# Display the result for the first pair of images
+plt.figure(figsize=(8, 4))
+plt.subplot(121), plt.imshow(img_matches_bf), plt.title('BruteForceMatcher - House vs ATU2')
+
+# Load new images
+img1_new = cv2.imread(r'C:\Lab9\Dail.jpg')
+img2_new = cv2.imread(r'C:\Lab9\Kitchen.jpg')
+
+# Convert the new images to grayscale
+gray1_new = cv2.cvtColor(img1_new, cv2.COLOR_BGR2GRAY)
+gray2_new = cv2.cvtColor(img2_new, cv2.COLOR_BGR2GRAY)
+
+# Find the keypoints and descriptors with ORB for the new images
+kp1_new, des1_new = orb.detectAndCompute(gray1_new, None)
+kp2_new, des2_new = orb.detectAndCompute(gray2_new, None)
+
+# Brute-Force Matcher for the new images
+matches_bf_new = bf.match(des1_new, des2_new)
+
+# Sort them in ascending order of distance
+matches_bf_new = sorted(matches_bf_new, key=lambda x: x.distance)
+
+# Draw matches for the new images
+img_matches_bf_new = cv2.drawMatches(img1_new, kp1_new, img2_new, kp2_new, matches_bf_new[:10], None, flags=2)
+
+# Display the result for the second pair of images
+plt.subplot(122), plt.imshow(img_matches_bf_new), plt.title('BruteForceMatcher - Dail vs Kitchen')
+
+# Adjust layout for better display
+plt.tight_layout()
+
+# Show the plots
 plt.show()
