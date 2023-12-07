205d53dbc5aa19580b7a50bd3d3ae8993c8ee9df
Ross Hannon <G00381859@gmit.ie>
2023-12-07 13:27:29 +0000

    L9 deep copy created

--- Lab9.py
+++ Lab9.py
@@ -35,17 +35,19 @@ plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
 plt.title('Harris Corners')
 plt.show()
 
-
+# Commented out the Harris corner detection code
+"""
 # Set the threshold for Harris corner detection
-#threshold_harris = 0.01
-# Loop through every element in the dst matrix
-#for i in range(len(dst)):
-    #for j in range(len(dst[i])):
-        # Check if the element is greater than the threshold
-        #if dst[i][j] > (threshold_harris * dst.max()):
-            # Draw a circle on the imgHarris image for Harris corners
-            #cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)  # Red color for Harris circles
+threshold_harris = 0.01
 
+# Loop through every element in the dst matrix
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        # Check if the element is greater than the threshold
+        if dst[i][j] > (threshold_harris * dst.max()):
+            # Draw a circle on the imgHarris image for Harris corners
+            cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)  # Red color for Harris circles
+"""
 
 # Perform corner detection using Shi-Tomasi algorithm
 maxCorners = 100  # Experiment with different values
@@ -64,7 +66,15 @@ for corner in corners:
     x, y = corner.ravel()
     cv2.circle(imgGFTT, (x, y), 3, (0, 255, 0), -1)  # Green color for GFTT circles
 
+# Create a deep copy of the original image for Shi-Tomasi corners
+imgShiTomasi = copy.deepcopy(imgOrig)
+
+# Draw circles on the image for each detected corner using Shi-Tomasi
+for corner in corners:
+    x, y = corner.ravel()
+    cv2.circle(imgShiTomasi, (x, y), 3, (255, 0, 0), -1)  # Blue color for Shi-Tomasi circles
+
 # Plot the image with Shi-Tomasi corners
-plt.imshow(cv2.cvtColor(imgGFTT, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB))
 plt.title('Shi-Tomasi Corners')
 plt.show()
