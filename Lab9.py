81002cf4f07db23d434ee424bc9fec8a580d32a6
Ross Hannon <G00381859@gmit.ie>
2023-12-07 13:42:49 +0000

    L9 ORB keypoints plotted

--- Lab9.py
+++ Lab9.py
@@ -1,14 +1,24 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+
+# Load the image in grayscale
 img = cv.imread(r'C:\Lab9\ATU1.jpg', cv.IMREAD_GRAYSCALE)
+
 # ORB (Oriented FAST and Rotated BRIEF)
 # Initiate ORB detector
 orb = cv.ORB_create()
-# find the keypoints with ORB
-kp = orb.detect(img,None)
-# compute the descriptors with ORB
+
+# Find the keypoints with ORB
+kp = orb.detect(img, None)
+
+# Compute the descriptors with ORB
 kp, des = orb.compute(img, kp)
-# draw only keypoints location,not size and orientation
-img2 = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
-plt.imshow(img2), plt.show()
+
+# Draw only keypoints location, not size and orientation
+img_with_keypoints = cv.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=0)
+
+# Plot the image with keypoints
+plt.imshow(img_with_keypoints, cmap='gray')
+plt.title('ORB Keypoints')
+plt.show()
